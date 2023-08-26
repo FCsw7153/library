dc18957da6c9c471b7caccd901b4964a301b0001
FCsw7153 <fc1694007153@gmail.com>
2023-08-26 18:14:42 +0800

    update

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-#include <link_list.c>
 
-int main() {
-    return 0;
+struct Node {
+    int Value;
+    struct Node *next;
+};
+
+// insertNode to insert data into link_list
+void insertNode(int i, struct Node *p) {
+    // new_link_list : new link_list to insert
+    // i : data in new_link_list
+    // p : link_list
+    struct Node *new_link_list;
+    new_link_list -> Value = i;
+    new_link_list -> next = p -> next;
+    p -> next = new_link_list -> next;
 }
+
